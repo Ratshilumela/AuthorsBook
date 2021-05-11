3686a016780436dd24bc8108e662074a50729c84
Ratshilumela <55391455+Ratshilumela@users.noreply.github.com>
2021-05-11 23:47:37 +0200

    Add files via upload

---
+++ BooksAssement/ASPNETMVCApplication/ASPNETMVCApplication/Controllers/BookController.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Web;
+using System.Linq;
+using System.Web.Mvc;
+using ASPNETMVCApplication.Models;
+
+namespace ASPNETMVCApplication.Controllers
+{
+    public class BookController : Controller
+    {
+        BookDBContext _db = new BookDBContext(); 
+
+        //
+        // GET: /Book/
+
+        public ActionResult Index()
+        {
+            var books = from book in _db.Books
+                        select book;
+
+            return View(books.ToList());
+        }
+
+        //
+        // GET: /Book/Details/5
+
+        public ActionResult Details(int au_id)
+        {
+            Book book = _db.Books.Find(au_id);
+
+            if (book == null)
+                return RedirectToAction("Index");
+
+            return View("Details", book); 
+        }
+
+        //
+        // GET: /Book/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        } 
+
+        //
+        // POST: /Book/Create
+
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            
+                Book book = new Book();
+                if (ModelState.IsValau_id)
+                {                    
+                    book.au_lname = collection["au_lname"].ToString();
+                    book.au_fname = collection["au_fname"].ToString();
+                    book.phone = Convert.ToDecimal(collection["phone"]);
+                    book.address = collection["address"].ToString();
+                    book.city = collection["city"].ToString();
+                    book.state = collection["state"].ToString();
+                    book.zip = collection["zip"].ToString();
+                    book.contract = collection["contract"].ToString();
+                
+
+                _db.Books.Add(book);
+                    _db.SaveChanges(); 
+
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    return View(book);
+                }
+                    
+        }
+        
+        //
+        // GET: /Book/Edit/5
+ 
+        public ActionResult Edit(int au_id)
+        {
+            Book book = _db.Books.Find(au_id);
+            if (book == null)
+                return RedirectToAction("Index");
+
+            return View(book); 
+        }
+
+        //
+        // POST: /Book/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(int au_id, FormCollection collection)
+        {
+            try
+            {
+                var book = _db.Books.Find(au_id);
+
+                UpdateModel(book);
+                _db.SaveChanges();               
+ 
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                ModelState.AddModelError("", "Edit Failure, see inner exception");
+                return View();
+            }
+        }
+
+        //
+        // GET: /Book/Delete/5
+ 
+        public ActionResult Delete(int au_id)
+        {
+            Book book = _db.Books.Find(au_id);
+            if (book == null)
+                return RedirectToAction("Index");
+            return View(book);
+        }
+
+        //
+        // POST: /Book/Delete/5
+
+        [HttpPost]
+        public ActionResult Delete(int au_id, FormCollection collection)
+        {
+            Book book = _db.Books.Find(au_id);
+            _db.Books.Remove(book);
+            _db.SaveChanges();
+
+            return RedirectToAction("Index"); 
+        }
+    }
+}
---
+++ BooksAssement/ASPNETMVCApplication/ASPNETMVCApplication/Models/Book.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Web;
+
+namespace ASPNETMVCApplication.Models
+{
+    public class Book
+    {
+        public Int64 au_id { get; set; }
+        public String au_lname { get; set; }
+        public String au_fname { get; set; }
+        public Decimal phone { get; set; }
+        public String address { get; set; }
+        public String city { get; set; }
+        public String state { get; set; }
+        public String zip { get; set; }
+        public String contract { get; set; }
+
+      
+    }
+}
